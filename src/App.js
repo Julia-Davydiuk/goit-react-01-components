8658cc081c5b9eaff1fb97bc108c2b52ea6cc31c
Julia-Davydiuk <70822407+Julia-Davydiuk@users.noreply.github.com>
2021-04-25 20:57:23 +0200

    начало

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Profile from './components/Profile';
+import user from './user.json';
+import Statistics from './components/Statistics';
+import statisticalData from './statistical-data.json';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile
+         name={user.name}
+         tag={user.tag}
+         location={user.location}
+         avatar={user.avatar}
+         stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={statisticalData} />
+      {/* <Statistics stats={statisticalData} /> */}
     </div>
-  );
-}
+  )
+};
 
 export default App;
